80986f941dcdd9f2d5dfe7f39fffe957bcd96b63
radek <rcirmira@gmail.com>
2025-02-18 15:23:55 +0000

    Remove redundant path annotation

--- user-service/src/main/java/com/db/system/user/resource/user/UserResourceImpl.java
+++ user-service/src/main/java/com/db/system/user/resource/user/UserResourceImpl.java
@@ -4,13 +4,11 @@ import com.db.system.user.data.model.User;
 import com.db.system.user.service.UserDetailsService;
 import jakarta.inject.Inject;
 import jakarta.inject.Singleton;
-import jakarta.ws.rs.Path;
 import jakarta.ws.rs.core.Response;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 @Singleton
-@Path("/api/v1/user")
 public class UserResourceImpl implements UserResource {
     private static final Logger LOG = LoggerFactory.getLogger(UserResourceImpl.class);
 
