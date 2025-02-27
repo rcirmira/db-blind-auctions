8af68f5c1802a6a3a801bcce0f7f70a980d9166d
radek <rcirmira@gmail.com>
2025-02-27 22:52:39 +0000

    Improve code coverage

--- auction-service/src/test/java/com/db/system/auction/resource/auction/AuctionResourceImplTest.java
+++ auction-service/src/test/java/com/db/system/auction/resource/auction/AuctionResourceImplTest.java
@@ -6,14 +6,14 @@ import com.db.system.auction.dao.AuctionDao;
 import com.db.system.auction.service.AuctionDetailsService;
 import com.db.system.auction.service.AuctionDetailsServiceImpl;
 import com.db.system.data.model.auction.Auction;
+import com.db.system.data.model.auction.AuctionAdd;
 import com.db.system.resource.auction.AuctionResource;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import java.util.List;
 
-import static org.testng.Assert.assertEquals;
-import static org.testng.Assert.assertNotNull;
+import static org.testng.Assert.*;
 
 public class AuctionResourceImplTest {
     private AuctionResource auctionResource;
@@ -28,9 +28,29 @@ public class AuctionResourceImplTest {
     }
 
     @Test
-    public void getAuctionsReturnsEmptyArrayTest() {
+    public void addAuctionSuccessTest() {
         List<Auction> auctions = auctionResource.getAuctions();
         assertNotNull(auctions);
         assertEquals(auctions.size(), 0);
+
+        AuctionAdd auctionAdd = new AuctionAdd();
+        auctionAdd.setName("someAuction");
+        auctionAdd.setMinPrice(100);
+        Auction auction = auctionResource.addAuction(auctionAdd);
+        assertNotNull(auction);
+        assertEquals(auction.getId(), 1);
+        assertEquals(auction.getName(), "someAuction");
+        assertEquals(auction.getMinPrice(), 100);
+        assertTrue(auction.isActive());
+
+        auctions = auctionResource.getAuctions();
+        assertNotNull(auctions);
+        assertEquals(auctions.size(), 1);
+        Auction auctionGet = auctions.get(0);
+        assertNotNull(auctionGet);
+        assertEquals(auctionGet.getId(), 1);
+        assertEquals(auctionGet.getName(), "someAuction");
+        assertNull(auctionGet.getMinPrice());
+        assertTrue(auctionGet.isActive());
     }
 }
